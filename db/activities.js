7ed4752f84a0a82d34d0038b1b40b176209f7279
Snjezana Jaksic <snjezana.jaksic.sj@gmail.com>
2023-01-30 11:34:51 -0500

    createActivity done

--- db/activities.js
+++ db/activities.js
@@ -3,31 +3,38 @@ const client = require('./client');
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+  const { rows: [ activity ] } = await client.query(`
+      INSERT INTO activities (name, description) 
+      VALUES($1, $2)
+      RETURNING *;
+    `, [name, description]);
+
+    return activity;
 }
 
-async function getAllActivities() {
-  // select and return an array of all activities
-}
+// async function getAllActivities() {
+//   // select and return an array of all activities
+// }
 
-async function getActivityById(id) {}
+// async function getActivityById(id) {}
 
-async function getActivityByName(name) {}
+// async function getActivityByName(name) {}
 
-async function attachActivitiesToRoutines(routines) {
-  // select and return an array of all activities
-}
+// async function attachActivitiesToRoutines(routines) {
+//   // select and return an array of all activities
+// }
 
-async function updateActivity({ id, ...fields }) {
-  // don't try to update the id
-  // do update the name and description
-  // return the updated activity
-}
+// async function updateActivity({ id, ...fields }) {
+//   // don't try to update the id
+//   // do update the name and description
+//   // return the updated activity
+// }
 
 module.exports = {
-  getAllActivities,
-  getActivityById,
-  getActivityByName,
-  attachActivitiesToRoutines,
+  //getAllActivities,
+  //getActivityById,
+  //getActivityByName,
+  //attachActivitiesToRoutines,
   createActivity,
-  updateActivity,
+  //updateActivity,
 };
