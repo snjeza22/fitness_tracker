46c1af8a8ec507fe4f0856a3436c490c15538973
Snjezana Jaksic <snjezana.jaksic.sj@gmail.com>
2023-02-12 23:19:56 -0500

    404 error

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 require("dotenv").config()
 const express = require("express")
-const app = express()
+const app = express();
+const apiRouter = require("./api")
 
 // Setup your Middleware and API Router here
+app.use(express.json())
+//app.use(express.urlencoded())
+app.use("/api", apiRouter)
+
+//error handler
+
+app.use((req, res, next)=>{
+  // next(createError(404));
+  res.status(404);
+  res.send({message: "404 Page Not Found"});
+})
 
 module.exports = app;
